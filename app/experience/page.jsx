e41cd4391f57d497b5f6fa3163781c54debe2223
Yashwanth Reddy <yashreddy1762@gmail.com>
2025-11-11 12:12:53 -0600

    first commit

--- app/experience/page.jsx
+++ app/experience/page.jsx
@@ -35,5 +35,27 @@ export default function Page(){return (<div><h1 className="text-3xl font-bold mb
 </ul>
 </div>
 
+<div>
+<h2 className="font-semibold text-xl">Casey’s General Store – Ankeny, IA</h2>
+<p className="italic text-gray-600">Java Developer | Feb 2015 – Feb 2017</p>
+<p>Retail Inventory and Order Management System.</p>
+<ul className="list-disc list-inside mt-2">
+<li>Built REST APIs and AngularJS frontends for inventory management.</li>
+<li>Implemented asynchronous communication via RabbitMQ for supplier order processing.</li>
+<li>Improved inventory accuracy by 30% through automation and optimization.</li>
+</ul>
+</div>
+
+<div>
+<h2 className="font-semibold text-xl">Capgemini – Chennai, India</h2>
+<p className="italic text-gray-600">Associate Software Engineer → Java Developer | Jul 2013 – Jan 2015</p>
+<p>Client Onboarding & Case Management Portal.</p>
+<ul className="list-disc list-inside mt-2">
+<li>Dveloped Spring MVC modules for client registration and service request tracking.</li>
+<li>Integrated Hibernate ORM with Oracle for data persistence.</li>
+<li>Enhanced system response time by 20% through query optimization.</li>
+</ul>
+</div>
+
 </section>
 </div>);}
--- app/skills/page.jsx
+++ app/skills/page.jsx
@@ -1,1 +1,7 @@
-export default function Page(){return (<div><h1 className='text-3xl font-bold mb-4'>Technical Skills</h1><p>Java, Spring Boot, React, Angular, Microservices, REST APIs, AWS, Docker, Jenkins, SQL/NoSQL, Hibernate, Git, Agile, CI/CD</p></div>);}
+export default function Page(){return (<div><h1 className='text-3xl font-bold mb-4'>Technical Skills</h1><p><li>Programming languages: Java (17/11), J2EE 1.8, Python, JavaScript</li>
+<li>Technologies & Frameworks: Spring Boot, Spring MVC, Spring Security, Hibernate, JDBC, JSP, JSTL, Node.js, Angular, ReactJS, Redux, RxJS, Material-UI, Bootstrap</li>
+<li>Database: Redis, Azure Blob Storage, Cassandra, Oracle, MySQL, MS SQL PL/SQL, S3</li>
+<li>Web Services: HTML5, CSS3, JavaScript, Angular, React JS, SOAP, REST</li>
+<li>Messaging: Azure Service Bus, Solr, RabbitMQ, Kafka</li>
+<li>CI/CD & DevOps: Jenkins, JIRA, Docker, Kubernetes</li>
+<li>Version Control System: Git, Perforce & SVN</li></p></div>);}
